71d1f4651ddc020af8ff98b83d615cde57188630
bhuppidhamii <dhamibhupendra3@gmail.com>
2025-09-07 17:48:45 +0530

    employee dto mapper

---
+++ src/main/java/bhuppi/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package bhuppi.ems_backend.mapper;
+
+import bhuppi.ems_backend.dto.EmployeeDto;
+import bhuppi.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getFirstName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
